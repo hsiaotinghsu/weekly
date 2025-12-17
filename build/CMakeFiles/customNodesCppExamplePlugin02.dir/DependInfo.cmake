17c06be60890dfc98b88e28982c18fab888c06ce
hsiaotinghsu <wxes8950228@gmail.com>
2025-12-17 10:00:41 +0800

    Add files via upload

---
+++ build/CMakeFiles/customNodesCppExamplePlugin02.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/auo/桌面/1216/aarch64/customNodesCppExamplePlugin02/customNodesCppExamplePlugin02.cpp" "CMakeFiles/customNodesCppExamplePlugin02.dir/customNodesCppExamplePlugin02.cpp.o" "gcc" "CMakeFiles/customNodesCppExamplePlugin02.dir/customNodesCppExamplePlugin02.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/customNodesCppExamplePlugin02.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/customNodesCppExamplePlugin02.dir/customNodesCppExamplePlugin02.cpp.o"
+  "CMakeFiles/customNodesCppExamplePlugin02.dir/customNodesCppExamplePlugin02.cpp.o.d"
+  "libcustomNodesCppExamplePlugin02.pdb"
+  "libcustomNodesCppExamplePlugin02.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/customNodesCppExamplePlugin02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
